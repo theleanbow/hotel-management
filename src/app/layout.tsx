5183201eb38625005b1f6ae4aae9cd4b70e831d7
theleanbow <theleanbow@gmail.com>
2024-01-29 19:15:41 +0000

    update header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
+// don't forget / after @ symbol, took me half an hour to see that symbol 
+import Header from '@/components/Header/Header';
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "700", "900"],
 style:['italic', 'normal'], variable:'--font-poppins'});
 
 export const metadata: Metadata = {
+  // works like head title on html
   title: "Hotel Mangement App",
   description: "Discover the best hotels",
 };
@@ -20,8 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main>
-          {children}
+        {/* imports header component */}
+      <Header /> 
+        <main className="font-normal">
+          {/* {children} */}
+          {/* ?Footer */}
           </main>
         
         </body>
